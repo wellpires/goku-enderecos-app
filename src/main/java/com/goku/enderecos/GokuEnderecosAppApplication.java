ea83832a266d5d37bd5502b85d29c870dd3ad313
wellpires <wellington.goncalves@live.com>
2020-04-07 11:00:05 -0300

    Commit inicial

---
+++ src/main/java/com/goku/enderecos/GokuEnderecosAppApplication.java
@@ -0,0 +1,13 @@
+package com.goku.enderecos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GokuEnderecosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuEnderecosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goku/enderecos/GokuEnderecosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goku.enderecos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GokuEnderecosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
